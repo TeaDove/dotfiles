92d01feeb119865cf3355f2daaeb3d7094360ae3
Peter Ibragimov <pibragimov@mts.ru>
2025-10-13 13:21:39 +0700

    auto: autocommit

--- pkg/cli/commands/git/run_git_auto.go
+++ pkg/cli/commands/git/run_git_auto.go
@@ -3,7 +3,6 @@ package git
 import (
 	"context"
 	"dotfiles/pkg/cli/utils"
-	"os/exec"
 
 	"github.com/pkg/errors"
 	"github.com/urfave/cli/v3"
@@ -15,18 +14,8 @@ func RunGitAuto(ctx context.Context, _ *cli.Command) error {
 		return errors.Wrap(err, "git add .")
 	}
 
-	_, err = utils.ExecCommand(ctx, "git", "commit", "-m", `"auto commit"`)
-	if err != nil {
-		return errors.Wrap(err, "git commit")
-	}
-
 	_, err = utils.ExecCommand(ctx, "git", "commit", "-m", "undefined")
 	if err != nil {
-		var exitErr *exec.ExitError
-		if !errors.As(err, &exitErr) || exitErr.ExitCode() != 1 {
-			return errors.Wrap(err, "git commit")
-		}
-
 		_, _ = utils.ExecCommand(ctx, "git", "commit", "-m", "undefined")
 	}
 
