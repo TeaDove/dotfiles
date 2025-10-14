f91f57f54c7d2be1a8907810411414e1671b51ec
Peter Ibragimov <pibragimov@mts.ru>
2025-10-14 22:04:26 +0700

    feat: add size to t and iu

--- pkg/cli/uuid.go
+++ pkg/cli/uuid.go
@@ -4,20 +4,36 @@ import (
 	"context"
 	"crypto/rand"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/google/uuid"
 	"github.com/urfave/cli/v3"
 )
 
-func CommandUUID(_ context.Context, _ *cli.Command) error {
-	fmt.Print(strings.ToUpper(uuid.New().String())) //nolint:forbidigo // is ok
+func getLen(command *cli.Command) uint {
+	arg, err := strconv.Atoi(command.Args().First())
+	if err != nil {
+		return 1
+	}
+
+	return uint(arg)
+}
+
+func CommandUUID(_ context.Context, command *cli.Command) error {
+	arg := getLen(command)
+	for range arg {
+		fmt.Print(strings.ToUpper(uuid.New().String())) //nolint:forbidigo // is ok
+	}
 
 	return nil
 }
 
-func CommandText(_ context.Context, _ *cli.Command) error {
-	fmt.Print(rand.Text()) //nolint:forbidigo // is ok
+func CommandText(_ context.Context, command *cli.Command) error {
+	arg := getLen(command)
+	for range arg {
+		fmt.Print(rand.Text()) //nolint:forbidigo // is ok
+	}
 
 	return nil
 }
