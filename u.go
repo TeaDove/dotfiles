0931ab8148cb458e5239d547d168c9482ca7c490
TeaDove <peter.ibragimov@gmail.com>
2025-12-16 18:12:36 +0400

    evening small fix from TeaDoves-MacBook-Air-M4.local

--- u.go
+++ u.go
@@ -9,6 +9,17 @@ import (
 )
 
 func main() {
+	// file, err := os.OpenFile(".profile.pprof", os.O_CREATE|os.O_WRONLY, 0666)
+	// if err != nil {
+	//	panic(err)
+	//}
+	// defer file.Close()
+	//
+	// err = pprof.StartCPUProfile(file)
+	// if err != nil {
+	//	panic(err)
+	//}
+	// defer pprof.StopCPUProfile()
 	err := cli.Run(context.Background())
 	if err != nil {
 		color.Red("Unexpected error during execution\n")
