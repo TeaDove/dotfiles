24c28f08d471848e74b439f196390e63c80a1b29
TeaDove <peter.ibragimov@gmail.com>
2026-08-23 17:57:19 +0300

    daily minor edit from TeaDoves-MacBook-Air-M4.local

--- extra/osx-install.sh
+++ extra/osx-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew install tmux git jql lsd dust lazygit fish zsh gopass 2fa curlie wget cloc curlie tree neovim bat lolcat
+brew install tmux git jql lsd dust lazygit fish zsh gopass 2fa curlie wget cloc curlie tree neovim bat lolcat kitty
 
 # Starship
 curl -sS https://starship.rs/install.sh | sh
