92f44bc4e101b5c7babd3ffc308a68ff62a268a1
TeaDove <teadove@yandex-team.ru>
2026-08-27 18:32:05 +0300

    evening minor fix from teadove-OSX

--- extra/osx-install.sh
+++ extra/osx-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew install tmux git jql lsd dust lazygit fish zsh gopass 2fa curlie wget cloc curlie tree neovim bat lolcat kitty terraform
+brew install tmux git jql yq lsd dust lazygit fish zsh gopass 2fa curlie wget cloc curlie tree neovim bat lolcat kitty terraform graphviz
 
 # Starship
 curl -sS https://starship.rs/install.sh | sh
