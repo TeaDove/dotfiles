8197ea21b23345bd08d495ff8f11559c650c4a4f
Peter Ibragimov <teadove@yandex-team.ru>
2026-09-14 18:17:13 +0300

    evening minor fix from teadove-OSX

--- dotfiles-configs/.config/fish/config.fish
+++ dotfiles-configs/.config/fish/config.fish
@@ -44,7 +44,7 @@ function cbox
 end
 
 # set     prefix=/opt/homebrew
-set gnubin_path "$(find -L "/opt/homebrew/opt" -name gnubin | sort | tr '\n' :)"
+set gnubin_path "(find -L "/opt/homebrew/opt" -name gnubin | sort | tr '\n' :)"
 
 set PATH $gnubin_path/opt/homebrew/bin /opt/homebrew/sbin $HOME/.local/bin /usr/local/go/bin $HOME/.cargo/bin $PATH $HOME/Library/Python/3.8/bin /opt/homebrew/bin $HOME/yandex-cloud/bin $HOME/ycp/bin $HOME/ydb/bin $HOME/go/bin $HOME/go/bin/darwin_amd64 /opt/homebrew/opt/libpq/bin
 set HOMEBREW_NO_AUTO_UPDATE 1
