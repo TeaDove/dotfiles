38b528a0ee943e29c7415712dedd413ddd9850fe
Peter Ibragimov <teadove@yandex-team.ru>
2026-09-14 18:22:22 +0300

    evening minor fix from teadove-OSX

--- extra/ubuntu-install.sh
+++ extra/ubuntu-install.sh
@@ -8,8 +8,9 @@ apt install -y python3 python3-pip python3-dev python3-setuptools python3-venv \
   build-essential make git net-tools curl wget vim neovim \
   fish zsh tmux neofetch btop golang-go
 
+snap install lsd
+
 curl -sS https://starship.rs/install.sh | sh
-# lsd
 
 git config --global credential.helper store
 
